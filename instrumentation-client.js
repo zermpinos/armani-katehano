ca1caaaddeefffaca6157262bfafc5e83006b5ae
zermpinos <panoszermpinos@gmail.com>
2026-03-17 23:31:39 +0200

    added sentry :)

---
+++ instrumentation-client.js
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c0d3e7ae1e21b60919ce7ff58a285d04@o4511061937946624.ingest.de.sentry.io/4511061940961360",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
