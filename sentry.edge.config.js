53b45b9bc30437e2a469c9ad76ba1d898dfb056c
zermpinos <130582198+zermpinos@users.noreply.github.com>
2026-03-18 08:42:16 +0200

    added sentry :)

---
+++ sentry.edge.config.js
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c0d3e7ae1e21b60919ce7ff58a285d04@o4511061937946624.ingest.de.sentry.io/4511061940961360",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
