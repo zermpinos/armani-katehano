3e1752bb3344ecd422b56360dcb575e486f5690d
zermpinos <130582198+zermpinos@users.noreply.github.com>
2026-03-25 04:47:10 +0200

    added meta html tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import "../styles/globals.css";
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="jLrtMQ8j8j_KaFR2e4nhtIpZ0DCSMtekcD2kbY4n2mc" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
