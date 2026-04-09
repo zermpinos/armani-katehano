bdc3d49b50741ead06c01f0269dde28073948871
zermpinos <130582198+zermpinos@users.noreply.github.com>
2026-04-10 00:16:54 +0300

    trying vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { Analytics } from '@vercel/analytics/next';
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="google-site-verification" content="jLrtMQ8j8j_KaFR2e4nhtIpZ0DCSMtekcD2kbY4n2mc" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
