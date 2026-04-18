b6295061550285842313b854a4e2a9a8bbcb1a9c
zermpinos <130582198+zermpinos@users.noreply.github.com>
2026-04-18 05:46:49 +0300

    feat(sentry): set both to safe values, added beforeSend/beforeSendTransaction scrubbing tok...

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -3,10 +3,36 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+
+const PII_PATTERNS = [
+  /token=[0-9a-f]+/gi,
+  /admin\/[^?#\s]+/gi,
+  /coach\/[^?#\s]+/gi,
+];
+
+function scrubUrl(url) {
+  return PII_PATTERNS.reduce(
+    (u, re) => u.replace(re, (m) => m.split("=")[0] + "=[redacted]"),
+    url
+  );
+}
+
+function scrubEvent(event) {
+  if (event.request?.url) {
+    event.request.url = scrubUrl(event.request.url);
+  }
+  if (event.request?.query_string) {
+    event.request.query_string = "[redacted]";
+  }
+  return event;
+}
+
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  sendDefaultPii: false, // GDPR Reasons
+  sendDefaultPii: false,
   tracesSampleRate: 0.1,
+  beforeSend: scrubEvent,
+  beforeSendTransaction: scrubEvent,
 });
 
 process.on("unhandledRejection", (reason) => {
