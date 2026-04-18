58243e40a99088ff2548386a6629ca9e41246305
zermpinos <130582198+zermpinos@users.noreply.github.com>
2026-04-18 19:26:03 +0300

    feat(eslint): added eslint-plugin-security@4.0.0 and eslint-plugin-no-unsanitized@4.1.5 wit...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
 // eslint.config.mjs
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import security from "eslint-plugin-security";
+import noUnsanitized from "eslint-plugin-no-unsanitized";
 
 const eslintConfig = [
   ...nextCoreWebVitals,
+  security.configs.recommended,
+  noUnsanitized.configs.recommended,
   {
     ignores: [
       "node_modules/**",
